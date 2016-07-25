d06b60467266081268deb280905fa484db90edac
Chris Waldron <chris@chrissolutions.com>
2016-07-25 21:13:39 +1200

    RouteCollection support.

--- WebServer/Routing/RouteCollection.cs
+++ WebServer/Routing/RouteCollection.cs
@@ -1,50 +1,36 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WebServer.Routing
 {
-    public class RouteCollection : IEnumerable<RouteData>
+    public class RouteCollection : IEnumerable<RouteEntry>
     {
-        //public RouteCollection()
-        //{
-        //    Environment = new Dictionary<string, object>();
-        //    Parameters = new DynamicDictionary();
-        //}
+        private readonly ConcurrentDictionary<string, RouteEntry> _routeEntries;
 
-        ///// <summary>
-        ///// Maps the specified route template and sets default route values.
-        ///// </summary>
-        ///// <returns>
-        ///// A reference to the mapped route.
-        ///// </returns>
-        ///// <param name="routes">A collection of routes for the application.</param><param name="name">The name of the route to map.</param><param name="routeTemplate">The route template for the route.</param><param name="defaults">An object that contains default route values.</param>
-        public RouteCollection MapRoute(string name, string routeTemplate, object defaults)
+        public RouteCollection()
         {
+            _routeEntries = new ConcurrentDictionary<string, RouteEntry>();
+        }
+
+        /// <summary>
+        /// Maps the specified route template and sets default route values.
+        /// </summary>
+        /// <param name="name">The name of the route to map.</param>
+        /// <param name="routeTemplate">The route template for the route.</param>
+        /// <param name="defaults">An object that contains default route values.</param>
+        /// <returns>A reference to the mapped route.</returns>
+        public RouteCollection MapRoute(string name, string routeTemplate, object defaults = null)
+        {
+            var entry = new RouteEntry(name, routeTemplate, defaults);
+            _routeEntries.AddOrUpdate(name, entry, (k, v) => entry);
             return this;
         }
 
-        //public dynamic Parameters { get; set; }
-
-        //public IDictionary<string, object> Environment { get; set; }
-
-        //public object Response { get; set; }
-
-        //public Action<string> Expiry { get; set; }
-
-        //public Action<string> Authorize { get; set; }
-        //public IEnumerator<RouteData> GetEnumerator()
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //IEnumerator IEnumerable.GetEnumerator()
-        //{
-        //    return GetEnumerator();
-        //}
-        public IEnumerator<RouteData> GetEnumerator()
+        public IEnumerator<RouteEntry> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return _routeEntries.Select(pair => pair.Value).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
---
+++ WebServer/Routing/RouteEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace WebServer.Routing
+{
+    public class RouteEntry
+    {
+        public RouteEntry(string name, string routeTemplate, object defaults)
+        {
+            Name = name;
+            Template = routeTemplate;
+            Defaults = defaults;
+        }
+
+        public string Name { get; }
+
+        public string Template { get; }
+
+        public object Defaults { get; }
+    }
+}
